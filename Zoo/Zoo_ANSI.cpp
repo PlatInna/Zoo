e55f1c005d8a6443e52f1d5007ebf310077012a6
Platon Yarkin <yarkinpy@gmail.com>
2021-01-28 13:45:38 -0600

    Coursera grader passed

--- Zoo/Zoo_ANSI.cpp
+++ Zoo/Zoo_ANSI.cpp
@@ -1,4 +1,4 @@
-﻿// Zoo.cpp : https://www.coursera.org/ C++ Development Fundamentals: Brown Belt, Week 3.
+// Zoo.cpp : https://www.coursera.org/ C++ Development Fundamentals: Brown Belt, Week 3.
 // Task: add unique_ptr to let Zoo works
 //
 
@@ -18,9 +18,6 @@ using namespace std;
 //using Zoo = vector<Animal>;
 using Zoo = vector<unique_ptr<Animal>>;
 
-// Эта функция получает на вход поток ввода и читает из него описание зверей.
-// Если очередное слово этого текста - Tiger, Wolf или Fox, функция должна поместить соответствующего зверя в зоопарк.
-// В противном случае она должна прекратить чтение и сгенерировать исключение runtime_error.
 Zoo CreateZoo(istream& in) {
     Zoo zoo;
     string word;
@@ -44,9 +41,6 @@ Zoo CreateZoo(istream& in) {
     return zoo;
 }
 
-// Эта функция должна перебрать всех зверей в зоопарке в порядке их создания
-// и записать в выходной поток для каждого из них результат работы виртуальной функции voice.
-// Разделяйте голоса разных зверей символом перевода строки '\n'.
 void Process(const Zoo& zoo, ostream& out) {
     for (const auto& animal : zoo) {
         out << animal->Voice() << "\n";
